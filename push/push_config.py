01ab6d71552d63a5f8d3ea56a7fb548b11160648
maxlv7 <admin@maxlv.org>
2019-08-31 17:49:04 +0800

    update config

--- push/push_config.py
+++ push/push_config.py
@@ -1,12 +1,12 @@
 qq_bot_base_url = "http://qq.artin.li:8080/api/push"
-backend_base_url = "http://192.168.140.129:5000/"
+backend_base_url = "http://127.0.0.1:5000/"
 
 key = 123456789
 
 update_url = backend_base_url + "api/v1/course/getCourse?update=True&verifycode="
 push_qqbot_url = qq_bot_base_url
 get_week_url = backend_base_url + "api/v1/util/getWeek"
-redis_url = "redis://192.168.140.129:6379/0"
+redis_url = "redis://127.0.0.1:5000:6379/0"
 
 db_ip = "192.168.31.187"
 db_username = "root"
